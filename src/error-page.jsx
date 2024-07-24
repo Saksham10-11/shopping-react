30669fedac32576ba1732104da371f16923ed13e
Saksham <Sakshamsaipatwarpkd@gmail.com>
2024-07-24 23:02:57 +0530

    add background

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -5,11 +5,13 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen w-full px-10 pb-20">
+      <h1 className="text-6xl my-2 font-bold">Oops!</h1>
+      <p className="text-2xl my-4">Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i className="text-xl italic my-4  text-gray-500">
+          {error.statusText || error.message}
+        </i>
       </p>
     </div>
   );
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      <a href="/check">Hello world!</a>
-    </h1>
+    <div className="w-screen h-screen bg-[url('/Images/main-background.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="flex flex-row">
+        <nav>
+          <img src="logo.png" alt="" />
+          <ul>
+            <li className="inline-block">
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <img src="cart.png" alt="" />
+        </nav>
+      </div>
+    </div>
   );
 };
 
