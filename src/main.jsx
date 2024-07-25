cc9b17837366fcbce9b817d317defa7c67123998
Saksham <Sakshamsaipatwarpkd@gmail.com>
2024-07-25 23:17:54 +0530

    create home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page.jsx";
 import App from "./routes/App.jsx";
+import Shop from "./routes/Shop.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+    ],
   },
 ]);
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { IoBag } from "react-icons/io5";
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-[url('/Images/main-background.jpg')] bg-cover bg-center bg-no-repeat">
-      <div className="flex flex-row">
-        <nav>
-          <img src="logo.png" alt="" />
-          <ul>
-            <li className="inline-block">
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shop">Shop</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <img src="cart.png" alt="" />
-        </nav>
+      <div className="bg-opacity-40 bg-gray-800 h-screen px-10">
+        <div className="flex flex-row px-40 space-x-10  *:items-center border-b-2 border-opacity-70 border-solid border-white">
+          <img src="./Images/logo.png" className="w-20 h-20 bg-transparent" />
+          <nav className="flex flex-1 justify-around">
+            <ul className="inline-block font-mono text-2xl font-semibold text-white *:inline-block pt-4 *:px-4">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/shop">Shop</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </nav>
+          <IoBag size={65} color="white" />
+        </div>
+      </div>
+      <div>
+        <Outlet />
       </div>
     </div>
   );
